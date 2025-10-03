352ed3fde2217c48b2e330dce50933a6baba9f88
rizkySA-PUTRA1996 <rizkysaputraaa3@gmail.com>
2025-10-03 15:43:10 +0800

    mengganti dan menghubungkan profil ke database

--- app/Models/admin/Pejabat.php
+++ app/Models/admin/Pejabat.php
@@ -2,23 +2,28 @@
 
 namespace App\Models\admin;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pejabat extends Model
 {
+    use HasFactory;
+
     protected $table = 'pejabat';
-    public $timestamps = false;
-    protected $primaryKey = 'nip';
+    protected $primaryKey = 'nip'; // Menggunakan NIP sebagai primary key
+    public $incrementing = false; // Karena NIP bukan auto-increment
     protected $keyType = 'string';
+    public $timestamps = false;
+
     protected $fillable = [
         'nip',
         'id_jabatan',
         'nama_pejabat',
         'kata_sambutan',
         'gambar',
-        'nip',
     ];
 
+    // Relasi ke tabel Jabatan
     public function jabatan()
     {
         return $this->belongsTo(Jabatan::class, 'id_jabatan');
