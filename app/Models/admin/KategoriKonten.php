dd7b853819fa181ad8abacd65556e9c7fcd337b2
NajuanJRS <najuan600@gmail.com>
2025-10-14 11:17:05 +0800

    Perbaikan bug github

--- app/Models/admin/KategoriKonten.php
+++ app/Models/admin/KategoriKonten.php
@@ -2,15 +2,11 @@
 
 namespace App\Models\admin;
 
-<<<<<<< HEAD
-=======
 use Illuminate\Database\Eloquent\Factories\HasFactory;
->>>>>>> 54734343a0ed76c05c4e664b6699a738ed34efab
 use Illuminate\Database\Eloquent\Model;
 
 class KategoriKonten extends Model
 {
-<<<<<<< HEAD
     protected $table = 'kategori_konten';
     public $timestamps = false;
     protected $primaryKey = 'id_kategori_konten';
@@ -26,25 +22,3 @@ class KategoriKonten extends Model
         return $this->hasMany(Konten::class, 'id_kategori_konten', 'id_kategori_konten');
     }
 }
-=======
-    use HasFactory;
-
-    protected $table = 'kategori_konten';
-    protected $primaryKey = 'id_kategori_konten';
-    public $timestamps = false;
-
-    protected $fillable = [
-        'nama_menu_kategori',
-        'judul_konten',
-        'icon_konten',
-        'slug_konten',
-    ];
-
-    // Relasi ke tabel Konten
-    public function konten()
-    {
-        return $this->hasOne(Konten::class, 'id_kategori_konten');
-    }
-}
-
->>>>>>> 54734343a0ed76c05c4e664b6699a738ed34efab
--- app/Models/admin/Konten.php
+++ app/Models/admin/Konten.php
@@ -2,15 +2,11 @@
 
 namespace App\Models\admin;
 
-<<<<<<< HEAD
-=======
 use Illuminate\Database\Eloquent\Factories\HasFactory;
->>>>>>> 54734343a0ed76c05c4e664b6699a738ed34efab
 use Illuminate\Database\Eloquent\Model;
 
 class Konten extends Model
 {
-<<<<<<< HEAD
     protected $table = 'konten';
     public $timestamps = false;
     protected $primaryKey = 'id_konten';
@@ -27,28 +23,6 @@ class Konten extends Model
 
     public function kategoriKonten()
     {
-        return $this->belongsTo(KategoriKonten::class, 'id_kategori_konten', 'id_kategori_konten');
-=======
-    use HasFactory;
-
-    protected $table = 'konten';
-    protected $primaryKey = 'id_konten';
-    public $timestamps = false;
-
-    protected $fillable = [
-        'id_kategori_konten',
-        'gambar1_konten',
-        'isi_konten1',
-        'gambar2_konten',
-        'isi_konten2',
-        'gambar3_konten',
-        'isi_konten3',
-    ];
-
-    // Relasi ke tabel KategoriKonten
-    public function kategori()
-    {
-        return $this->belongsTo(KategoriKonten::class, 'id_kategori_konten');
->>>>>>> 54734343a0ed76c05c4e664b6699a738ed34efab
+        return $this->belongsTo(kategoriKonten::class, 'id_kategori_konten', 'id_kategori_konten');
     }
 }
