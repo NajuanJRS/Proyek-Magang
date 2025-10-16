1116e218f4b32041c13c23ee53d8eddaf9c5c12d
NajuanJRS <najuan600@gmail.com>
2025-10-16 14:20:58 +0800

    Fix bug

--- app/Models/admin/Galeri.php
+++ app/Models/admin/Galeri.php
@@ -7,14 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Galeri extends Model
 {
     protected $table = 'galeri';
-<<<<<<< HEAD
-    public $timestamps = false;
-    protected $primaryKey = 'id_galeri';
-    protected $fillable = [
-        'id_user',
-        'judul',
-        'gambar',
-=======
     protected $primaryKey = 'id_galeri';
     public $timestamps = false;
 
@@ -22,7 +14,6 @@ class Galeri extends Model
         'id_user',
         'gambar',
         'judul',
->>>>>>> 8efaa07de001f916471b4e563bd96870d09acc23
         'tanggal_upload',
     ];
 }
